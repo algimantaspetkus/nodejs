8db353105dd952e0c1b73f78826a53f13fadb707
algimantaspetkus <algimantas.petkus@doloop.com>
2023-05-25 06:45:11 +0300

    commit

--- njs_11_12/server.js
+++ njs_11_12/server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-const { USER, PASSWORD, DATABASE, HOST } = process.env;
+const { USER, PASSWORD, HOST, PORT, DATABASE } = process.env;
 const DBURL = `mongodb+srv://${USER}:${PASSWORD}@${HOST}/${DATABASE}?retryWrites=true&w=majority`;
 const connectDb = new MongoClient(DBURL);
 
@@ -15,9 +15,40 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/memberships', async (req, res) => {
+  try {
+    await connectDb.connect();
+    const memberships = await connectDb
+      .db('cao')
+      .collection('services')
+      .find()
+      .toArray();
+    res.status(200).json(memberships);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.delete('/memberships/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await connectDb.connect();
+    await connectDb
+      .db('cao')
+      .collection('services')
+      .deleteOne({ _id: new ObjectId(id) });
+    res.status(200).json({ message: 'Membership deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.get('/addmembership', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'addmembership.html'));
+});
+
 app.get('/', (req, res) => {
-  console.log('smt');
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(process.env.PORT);
+app.listen(PORT);
